4d227f342f5fd9eda4bd3be0b02608a6913dbdba
Bluishres <manuelsp7@outlook.es>
2021-03-07 22:00:36 +0100

    Order-SPM

---
+++ src/main/java/org/iesvalleinclan/storeorder/GestionPedidosApplication.java
@@ -0,0 +1,11 @@
+package org.iesvalleinclan.storeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPedidosApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPedidosApplication.class, args);
+	}
+}
